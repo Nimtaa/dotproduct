6812a8739a79ef00df28cc3f5e88c69cc599b8de
Nimtaa <nima.tavassoli2@gmail.com>
2020-11-07 01:45:01 +0330

    list ui added

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,57 @@ import React, {Component} from 'react';
 import axios from 'axios';
 
 import ProductCard from './components/ProductCard';
+import {List} from 'antd';
 
 class App extends Component{
-	state = {
-		response: {}
-	};
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			products: [],
+			cards: []
+		};
+		this.makeCardList = this.makeCardList.bind(this);
+	}
 
 	componentDidMount(){
 		axios.get('/products').then((res) =>{
 			const data = res.data;
 			console.log(data)
-			this.setState({response: data[0]});
+			this.setState({products: data}, function(){
+				this.makeCardList();
+			});
 		})
 	}
+
+	makeCardList(){
+		const cards = [];
+		this.state.products.forEach(p => {
+			cards.push(<ProductCard 
+				name={p.name}
+				description = {p.description}
+				vote = {p.vote}
+				_id = {p._id}/>)
+		});
+		this.setState({cards: cards}, function(){
+			console.log(this.state.cards);
+		});
+	}
+
+
 	render(){
 		return (
 			<div className="App">
-				<ProductCard 
-				name={this.state.response.name}
-				description = {this.state.response.description}
-				vote = {this.state.response.vote}
-				_id = {this.state.response._id}/>
+				<List
+				dataSource={this.state.products}
+				renderItem={p => (
+						<ProductCard 
+						name={p.name}
+						description = {p.description}
+						vote = {p.vote}
+						_id = {p._id} />
+					)}
+				/>
 			</div>
 		)
 	}
